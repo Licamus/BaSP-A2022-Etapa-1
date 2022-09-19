e30c90a06f827b7b8a04d25497c738f0a77c637d
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-09-19 18:32:35 -0300

    BaSP-W04-2022: ex03- Finish exercise 3

---
+++ Semana-04/exercises/03-arrays.js
@@ -0,0 +1,62 @@
+//Exercise a
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(months[4]);
+console.log(months[10]);
+
+//Exercise b
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.sort();
+
+console.log(months);
+
+//Exercise c
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.unshift('Extra1');
+months.push('Extra2');
+console.log(months);
+
+//Exercise d
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.shift();
+months.pop();
+
+console.log(months);
+
+//Exercise e
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+months.reverse();
+
+console.log(months);
+
+//Exercise f
+
+var string1, months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+string1=months.join('-');
+
+console.log(string1);
+
+//Exercise g
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+"Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var monthsCut = months.slice(4,11);
+
+console.log(monthsCut);

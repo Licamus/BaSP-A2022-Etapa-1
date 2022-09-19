378993d9dc6f1230d38a9622136b2f97b76e5d81
Maximiliano Licarzze <maxilicarzze@gmail.com>
2022-09-19 17:15:10 -0300

    BaSP-W04-2022: ex01- finished

---
+++ Semana-04/exercises/01-variables-and-operators.js
@@ -0,0 +1,25 @@
+//Exercise a
+var num1=2 , num2=3 , num3;
+
+console.log(num1);
+console.log(num2);
+
+num3=num1+num2;
+
+console.log(num3);
+
+//Exercise b
+
+var string1='Hello', string2='there' , string3;
+
+string3=string1+string2;
+
+console.log(string3);
+
+//Exercise c
+
+var string1='Hello', string2='there' , string3;
+
+string3=string1.length+string2.length;
+
+console.log(string3);
